e56f45ef2cf2e242d6e286103e256e37e9dd1f4e
Du Yanning <duyanning@gmail.com>
2017-09-10 18:49:42 +0800

    failed to deal space in filename

---
+++ test/space in folder/README.txt
@@ -0,0 +1,1 @@
+I failed to find a way to deal space in filename while keeping portable between linux and windows.
---
+++ test/space in folder/main.cpp
@@ -0,0 +1,9 @@
+#include "std.h" // precompile std.h
+
+using namespace std;
+
+int main()
+{
+    cout << "welcome to cpps" << endl;
+    return 0;
+}
