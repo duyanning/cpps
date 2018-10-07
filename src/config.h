b37871f25fdb4d3e41bd0c04ce527ad20d8ee3d3
Du Yanning <duyanning@gmail.com>
2018-10-07 21:40:00 +0800

    fix epm project

---
+++ src/config.h
@@ -0,0 +1,51 @@
+#ifndef STD_H
+#define STD_H
+
+
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+
+
+#define HAVE_REGEX
+
+#ifdef HAVE_REGEX
+#include <regex>
+#else
+#include <boost/regex.hpp>
+#endif
+
+
+#ifndef _WIN32
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#endif
+
+#include "MiniLogger.h"
+
+using namespace std;
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
+namespace al = boost::algorithm;
+
+
+#endif // STD_H
