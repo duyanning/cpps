45e92f2123cf628cfe1e5d57bb8dbb6ca918802d
Du Yanning <duyanning@gmail.com>
2022-01-16 21:16:13 +0800

    修复cpps未向脚本传递所有环境变量的bug

--- src/run.cpp
+++ src/run.cpp
@@ -11,10 +11,37 @@ void run()
 		char** script_argv = original_argv + script_pos;
 
 
-		//const char* env[] = { R"(PATH=F:\vcpkg\installed\x86-windows\bin)", nullptr };
-		const char* const env[] = { exec_cmd_env_vars.c_str(),
-			//R"(PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.21.27702\bin\HostX86\x86)",
-			nullptr };
+		//const char* const env[] = { exec_cmd_env_vars.c_str(),
+		//	nullptr };
+
+		// 复制父进程(cpps)的环境变量
+		char** env;
+		{
+			extern char** environ;
+			// 测算environ所指数组的长度
+			int n = 0;
+			for (char** p = environ; *p; p++) {
+				n++;
+			}
+
+			// 为复制环境变量分配内存
+			env = new char* [n + 1 + 1]; // 1个是exec_cmd_env_vars, 1个是最后的nullptr
+
+			// 复制
+			int i = 0;
+			for (char** p = environ; *p; p++, i++) {
+				//strcpy(env[i], *p);
+				env[i] = *p; // 直接指向父进程的环境变量
+			}
+			assert(i == n);
+
+			// 只为唯一新增的环境变量分配控件
+			env[i] = new char[exec_cmd_env_vars.length()+1]; // 这片内存没有释放的必要
+			strcpy(env[i], exec_cmd_env_vars.c_str());
+			env[i+1] = nullptr;
+
+		}
+		
 
 #ifdef _WIN32
 		for (int i = 0; i < script_argc; i++) {
@@ -35,6 +62,8 @@ void run()
 #else
 		execve(exe_path.c_str(), script_argv, const_cast<char* const *>(env));
 #endif
+
+		//delete[] env;  // 没必要, 压根执行不到这
 	}
 	else if (run_by == "system") {
 		MINILOG0("run using system()");
